4e7e0c8591025040aeb14b0890b95d3da1027fff
DNX-MD <dnxdnx530@gmail.com>
2025-04-18 23:21:16 +0530

    ❮ Lod-xxx-md ❯

---
+++ settings.js
@@ -0,0 +1,24 @@
+global.message_read = false;
+global.botname = "LOD-XXX-MD";
+global.autosviews = true;
+global.autotyping = true;
+global.ANTI_DELETE = true;
+global.SESSION_ID = 'GENUX-67f0c025ca221c68d46e7a55';
+global.prefix = ".";
+global.WELCOME = false;
+global.owner = "94727163302@s.whatsapp.net";
+global.owner_number = "94727163302";
+global.ownername = "CYBER DINU ID";
+
+module.exports = {
+  OWNER_NAME: 'CYBER DINU ID',
+  OWNER_NUMBER: ['94727163302', '94774589636'],
+  BOT_NAME: '',
+  API_KEY: 'GENUX-SANDARUX',
+  PREFIX: '.',
+  ANTI_DELETE: true,
+  AUTO_READ_MESSAGE: true,
+  AUTO_READ_STATUS: true,
+  AUTO_TYPING: true,
+  WELCOME: false
+};
